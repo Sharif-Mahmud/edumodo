e9e19c6fcd3f58a1fde3cc112757d420117362eb
Sharif-Mahmud <sharif230@gmail.com>
2024-02-27 13:50:44 +0600

    Complete project done

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$('.sharif').owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: true,
+    nav: true,
+    dots: false,
+    mouseDrag: true,
+    autoplayHoverPause: true,
+    autoplaySpeed: 1000,
+    smartSpeed: 1500,
+
+});
